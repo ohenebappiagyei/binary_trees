d90c6f9af8dbd237dc07477391be220248982277
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-03 12:41:01 -0800

    16 corrected

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,8 +20,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		{
 			int perf_left = binary_tree_is_perfect(tree->left);
 			int perf_right = binary_tree_is_perfect(tree->right);
+
 			return (perf_left && perf_right);
-		}		
+
+		}
 	}
 		return (0);
 }
