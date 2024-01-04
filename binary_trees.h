688a12db1f1d7619087cd3ea5a88f92066a2b693
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-04 11:39:12 -0800

    task 22

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 int is_complete(const binary_tree_t *tree, size_t index, size_t node_count);
 size_t binary_tree_size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
