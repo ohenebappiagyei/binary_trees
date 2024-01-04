8c63e9f000b969d96d09988a0b742f9ac931302c
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-04 10:31:18 -0800

    task 21 fixed

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,34 +1,49 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_levelorder - Traverse a binary tree using level-order traversal
+ * binary_tree_levelorder - Traverses a binary tree using level-order raversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
- * @Description: This function traverses the binary tree using level-order
- * traversal and applies the given function to each node's value.
 */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	/* Assuming a reasonable maximum size for the queue */
-	binary_tree_t *queue[10000];
-	int front = 0, rear = 0;
+	binary_tree_t **queue = NULL;
+	size_t front = 0, rear = 0;
+
+	queue = malloc(sizeof(binary_tree_t *));
+
+	if (queue == NULL)
+		return;
 
 	queue[rear++] = (binary_tree_t *)tree;
 
 	while (front < rear)
 	{
 		binary_tree_t *current = queue[front++];
+
 		func(current->n);
 
 		if (current->left != NULL)
+		{
+			queue = realloc(queue, (rear + 1) * sizeof(binary_tree_t *));
+
+			if (queue == NULL)
+				return;
 			queue[rear++] = current->left;
+		}
 
 		if (current->right != NULL)
+		{
+			queue = realloc(queue, (rear + 1) * sizeof(binary_tree_t *));
+			if (queue == NULL)
+				return;
 			queue[rear++] = current->right;
+		}
 	}
+
+	free(queue);
 }
