f3ab8c2cea61f64e5c81dcba04f9953649ad4d28
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-04 10:05:44 -0800

    task 21 complete

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int binary_tree_is_descendant(const binary_tree_t *ancestor, const binary_tree_t *descendant);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+int is_complete(const binary_tree_t *tree, size_t index, size_t node_count);
+size_t binary_tree_size(const binary_tree_t *tree);
+
 
 #endif /* _BINARY_TREES_H_ */
